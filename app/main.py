6cfd182fa68200630ffad58112c3a1feedd9bfd1
kahpereira <kamilapereira@outlook.com>
2024-11-05 08:23:58 -0300

    feat: add NotFound exception and its handler

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from app.routers import product
+from app.utils.exceptions import NotFound, unicorn_exception_handler
 
 app = FastAPI()
 
 app.include_router(product.router)
+app.add_exception_handler(NotFound, unicorn_exception_handler)
