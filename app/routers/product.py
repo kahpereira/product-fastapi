b1f18735328598273b7e2d5b19496ae737dc1fbc
kahpereira <kamilapereira@outlook.com>
2024-11-05 08:26:52 -0300

    feat: update routes to raise NotFound exception and use helper function

--- app/routers/product.py
+++ app/routers/product.py
@@ -1,9 +1,9 @@
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, status
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
-from app.database.dependencies import get_db
 from app.models.product import Product as ProductModel
 from app.schemas.product import ProductBase, ProductResponse
+from app.utils import get_db, commit_and_refresh, NotFound
 
 router = APIRouter()
 
@@ -11,9 +11,7 @@ router = APIRouter()
 async def create_product(product: ProductBase, db: AsyncSession = Depends(get_db)):
   db_product = ProductModel(**product.model_dump())
   
-  db.add(db_product)
-  await db.commit()
-  await db.refresh(db_product)
+  await commit_and_refresh(db, db_product)
   return db_product
 
 @router.get('/product/', response_model=list[ProductResponse], status_code=status.HTTP_200_OK)
@@ -26,28 +24,26 @@ async def get_all_products(db: AsyncSession = Depends(get_db)):
 async def get_product(product_id: int, db: AsyncSession = Depends(get_db)):
     db_product = await db.get(ProductModel, product_id)
     if not db_product:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Product not found")
+        raise NotFound("Product")
     return db_product
   
 @router.put("/product/{product_id}", response_model=ProductResponse, status_code=status.HTTP_200_OK)
 async def update_product(product_id: int, product: ProductResponse, db: AsyncSession = Depends(get_db)):
     db_product = await db.get(ProductModel, product_id)
     if not db_product:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Product not found")
+        raise NotFound("Product")
     
     for key, value in product.model_dump().items():
         setattr(db_product, key, value)
     
-    db.add(db_product)
-    await db.commit()
-    await db.refresh(db_product)
+    await commit_and_refresh(db, db_product)
     return db_product
   
 @router.delete("/product/{product_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_product(product_id: int, db: AsyncSession = Depends(get_db)):
     db_product = await db.get(ProductModel, product_id)
     if not db_product:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Product not found")
+        raise NotFound("Product")
     
     await db.delete(db_product)
     await db.commit()
